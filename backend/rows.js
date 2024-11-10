babf79840aabed316859a8fe170a0506d93fe166
Jurica Laljak <jurica.laljak@fer.unizg.hr>
2024-11-10 18:08:12 +0100

    Add files via upload

---
+++ backend/rows.js
@@ -0,0 +1,15 @@
+rows = {"games.name" : "str", 
+        "games.release_date" : "date", 
+        "games.developer" : "str",
+        "games.publisher" : "str",
+        "platforms.platform" : "str",
+        "games.genre" : "str",
+        "games.price" : "num",
+        "games.metascore" : "num",
+        "games.has_singleplayer" : "bool",
+        "games.has_multiplayer" : "bool",
+        "dlcs.dlc_name" : "str",
+        "dlcs.dlc_price" : "num",
+        "dlcs.dlc_release_date" : "date"}
+
+module.exports = rows
